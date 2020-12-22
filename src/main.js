1c39acfb64df5a98af5c2d06142e38f5140323a6
HLonlin <1021944439@qq.com>
2020-12-22 18:53:14 +0800

    增加tapList（底部导航栏） 修改项目依赖配置文件 移除normalize.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 
-import 'normalize.css/normalize.css'; // A modern alternative to CSS resets
 import '@/assets/styles/default.css';
 
 import tool from '@/assets/tools';
---
+++ src/views/compoents/tapList/tapList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tapList_container"></div>
+</template>
+
+<script>
+export default {
+  name: "tapList",
+  data() {
+    return {};
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
