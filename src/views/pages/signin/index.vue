82e044b7267f770abb396a3f8cc80c6c55e06bac
HLonlin <1021944439@qq.com>
2020-12-25 10:15:09 +0800

    签到顶部面板动画效果

--- src/views/pages/signin/index.vue
+++ src/views/pages/signin/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="container">
     <div class="signin_topPanel">
-      <div class="signin_roundShadow">
-        <div class="signin_roundWhite">
-          <div class="signin_round">签到</div>
-        </div>
+      <div class="signin_roundWhite">
+        <div class="signin_round">{{ is_Signin ? "已签到" : "签到" }}</div>
       </div>
       <div class="signin_position">
         <i class="iconItem icon_dingwei signin_icon"></i>
-        广东省广州市越秀区小北路22号
+        {{ currentPosition }}
       </div>
     </div>
   </div>
@@ -18,7 +16,10 @@
 export default {
   name: "signin",
   data() {
-    return {};
+    return {
+      is_Signin: false,
+      currentPosition: "广东省广州市越秀区小北路22号"
+    };
   },
   beforeCreate() {},
   created() {},
@@ -40,25 +41,51 @@ export default {
   height: 200px;
   background-color: #0090d7;
 }
-.signin_roundShadow {
+.signin_roundWhite {
   position: absolute;
   top: 20px;
   left: 50%;
   transform: translateX(-50%);
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  background-color: rgba(255, 255, 255, 0.5);
-}
-.signin_roundWhite {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   width: 90px;
   height: 90px;
   border-radius: 50%;
   background-color: #ffffff;
+}
+@keyframes cd-pulse {
+  0% {
+    transform: scale(1);
+    box-shadow: inset 0 0 10px 3px rgba(255, 255, 255, 1);
+  }
+  50% {
+    box-shadow: inset 0 0 10px 3px rgba(255, 255, 255, 0.5);
+  }
+  100% {
+    transform: scale(1.4);
+    box-shadow: inset 0 0 10px 3px rgba(0, 0, 0, 0);
+  }
+}
+.signin_roundWhite::before {
+  content: "";
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  border-radius: inherit;
+  background-color: transparent;
+  animation: cd-pulse 2s infinite;
+}
+.signin_roundWhite::after {
+  content: "";
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  border-radius: inherit;
+  background-color: transparent;
+  transform: scale(1.1);
+  box-shadow: inset 0 0 10px 5px rgba(255, 255, 255, 0.5);
 }
 .signin_round {
   position: absolute;
@@ -69,7 +96,7 @@ export default {
   height: 80px;
   border-radius: 50%;
   border: 1px solid #0090d7;
-  background-color: #ffffff;
+  background-color: transparent;
   color: #0090d7;
   text-align: center;
   line-height: 80px;
