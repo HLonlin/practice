3a7e473dcba28f8fefb3994116f7def3fc06e34f
HLonlin <1021944439@qq.com>
2020-12-29 16:31:07 +0800

    添加登录页

---
+++ src/views/pages/login/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login_container"></div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {};
+  },
+  beforeCreate() {},
+  created() {
+    let that = this;
+    setTimeout(() => {
+      let url = that.$route.query.redirect || "/";
+      this.$store.commit("addState", {
+        key: "tokenid",
+        val: url
+      });
+      that.$router.replace(url);
+    }, 1000);
+  },
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  methods: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
