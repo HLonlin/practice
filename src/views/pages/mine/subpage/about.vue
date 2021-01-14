cea790bb7acd6c197f973b399e3e9d85ab8f5392
HLonlin <1021944439@qq.com>
2021-01-14 15:59:30 +0800

    移动端关于菜单配置

--- src/views/pages/mine/subpage/about.vue
+++ src/views/pages/mine/subpage/about.vue
@@ -11,6 +11,22 @@
         @click-left="onClickLeft"
       />
     </div>
+    <div class="about_list">
+      <a
+        v-for="(item, i) in aboutList"
+        :key="i"
+        class="about_item"
+        :class="{ lastOne: i == aboutList.length - 1 }"
+        :href="item.value"
+      >
+        <img
+          class="about_logo"
+          src="../../../../assets/images/icon.png"
+          alt=""
+        />
+        <div class="about_text">{{ item.text }}</div>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -18,10 +34,14 @@
 export default {
   name: "about",
   data() {
-    return {};
+    return {
+      aboutList: []
+    };
   },
   beforeCreate() {},
-  created() {},
+  created() {
+    this.getAboutList();
+  },
   beforeMount() {},
   mounted() {},
   beforeUpdate() {},
@@ -31,13 +51,51 @@ export default {
   methods: {
     onClickLeft: function() {
       this.$router.go(-1);
+    },
+    // 加载关于菜单列表
+    getAboutList: function() {
+      let that = this;
+      that.$axios.post(that.$api.about, {}).then(res => {
+        let data = res.data;
+        for (let i = 0, imax = res.data.length; i < imax; i++) {
+          that.$set(that.aboutList, i, data[i]);
+        }
+      });
     }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.about_list {
+  box-sizing: border-box;
+  padding: 0px 1rem;
+}
+.about_item {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 18px 0px;
+  border-bottom: 1px solid #eeeeee;
+}
+.lastOne {
+  border: none;
+}
+.about_logo {
+  width: 1.25rem;
+  height: 1.25rem;
+}
+.about_text {
+  font-size: 0.875rem;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 400;
+  color: #0090d8;
+  box-sizing: border-box;
+  padding: 0px 0px 0px 0.625rem;
+}
+</style>
 <style>
 .about_container .van-nav-bar {
   background-color: #0090d8;
