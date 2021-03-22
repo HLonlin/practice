3328a7f90f1b85e979332e0133bc2bb8ba8f834a
HLonlin <1021944439@qq.com>
2021-03-22 10:07:01 +0800

    学生信息修改记录列表分类

--- src/views/pages/mine/subpage/updaterecord.vue
+++ src/views/pages/mine/subpage/updaterecord.vue
@@ -11,51 +11,49 @@
         @click-left="onClickLeft"
       />
     </div>
-    <div class="recordList_listPanel">
-      <van-pull-refresh v-model="refreshing" @refresh="dropDownRefresh">
-        <van-list
-          v-model="loading"
-          :finished="finished"
-          finished-text="没有更多了"
-          @load="onLoad"
-        >
-          <!-- <router-link
-            v-for="(item, i) in recordList"
-            :key="i"
-            :to="{
-              path: 'recorddetail',
-              query: { wf_docUnid: JSON.stringify(item.WF_DocUnid) }
-            }"
-            class="recordList_listItem"
-          > -->
-          <router-link
-            v-for="(item, i) in recordList"
-            :key="i"
-            :to="{
-              path: 'recorddetail',
-              query: { detail: JSON.stringify(item) }
-            }"
-            class="recordList_listItem"
+    <van-tabs v-model="active" color="#0090d8" :sticky="true" offset-top="44px">
+      <van-tab v-for="(itemObj, j) in tabList" :key="j" :title="itemObj.title">
+        <div class="recordList_listPanel">
+          <van-pull-refresh
+            v-model="itemObj.refreshing"
+            @refresh="dropDownRefresh(j)"
           >
-            <div class="recordList_listTitle">
-              修改记录
-            </div>
-            <div class="recordList_listLabel">
-              <div class="recordList_listAuthor">
-                审核状态:{{
-                  item.isty == "是"
-                    ? "已通过"
-                    : item.isty == "否"
-                    ? "未通过"
-                    : "待审核"
-                }}
-              </div>
-              <div class="recordList_listDate">{{ item.date }}</div>
-            </div>
-          </router-link>
-        </van-list>
-      </van-pull-refresh>
-    </div>
+            <van-list
+              v-model="itemObj.loading"
+              :finished="itemObj.finished"
+              finished-text="没有更多了"
+              @load="onLoad(j)"
+            >
+              <router-link
+                v-for="(item, i) in itemObj.recordList"
+                :key="i"
+                :to="{
+                  path: 'recorddetail',
+                  query: { detail: JSON.stringify(item) }
+                }"
+                class="recordList_listItem"
+              >
+                <div class="recordList_listTitle">
+                  修改记录
+                </div>
+                <div class="recordList_listLabel">
+                  <div class="recordList_listAuthor">
+                    审核状态:{{
+                      item.isty == "是"
+                        ? "已通过"
+                        : item.isty == "否"
+                        ? "未通过"
+                        : "待审核"
+                    }}
+                  </div>
+                  <div class="recordList_listDate">{{ item.date }}</div>
+                </div>
+              </router-link>
+            </van-list>
+          </van-pull-refresh>
+        </div>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
@@ -65,12 +63,51 @@ export default {
   data: function() {
     return {
       userData: Object,
+      tabList: [
+        {
+          title: "全部",
+          refreshing: false,
+          loading: false, // 加载状态
+          finished: false,
+          pageIndex: 1,
+          pageSize: 10,
+          recordList: []
+        },
+        {
+          title: "待审核",
+          refreshing: false,
+          loading: false, // 加载状态
+          finished: false,
+          pageIndex: 1,
+          pageSize: 10,
+          recordList: []
+        },
+        {
+          title: "已通过",
+          refreshing: false,
+          loading: false, // 加载状态
+          finished: false,
+          pageIndex: 1,
+          pageSize: 10,
+          recordList: []
+        },
+        {
+          title: "未通过",
+          refreshing: false,
+          loading: false, // 加载状态
+          finished: false,
+          pageIndex: 1,
+          pageSize: 10,
+          recordList: []
+        }
+      ],
       recordList: [],
       loading: false, // 加载状态
       finished: false, // 是否已加载全部
       refreshing: false,
       pageIndex: 1,
-      pageSize: 10
+      pageSize: 10,
+      active: 0
     };
   },
   methods: {
@@ -84,41 +121,37 @@ export default {
       }
     },
     // 加载记录列表
-    onLoad() {
-      let that = this,
-        data = {};
-      if (that.userData.isTeacher) {
-        data = {
-          cardid: that.$route.query.cardid
-        };
-      }
-      that.$axios.post(that.$api.recordList, data).then(res => {
-        that.loading = false;
-        that.pageIndex = that.pageIndex + 1;
-        let recordList = res.data;
-        for (let i = 0, imax = recordList.length; i < imax; i++) {
-          recordList[i].date = that.$tool.getFullDate(
-            recordList[i].WF_Created.replace(/-/g, "/")
-          );
-          that.recordList.push(recordList[i]);
-        }
-        that.finished = true;
-      });
+    onLoad(index) {
+      let that = this;
+      that.$axios
+        .post(that.$api.recordList, index == 0 ? {} : { status: index })
+        .then(res => {
+          that.tabList[index].loading = false;
+          that.tabList[index].pageIndex = that.tabList[index].pageIndex + 1;
+          let recordList = res.data;
+          for (let i = 0, imax = recordList.length; i < imax; i++) {
+            recordList[i].date = that.$tool.getFullDate(
+              recordList[i].WF_Created.replace(/-/g, "/")
+            );
+            that.tabList[index].recordList.push(recordList[i]);
+          }
+          that.tabList[index].finished = true;
+        });
     },
     // 下拉刷新
-    dropDownRefresh() {
-      if (this.refreshing) {
+    dropDownRefresh(index) {
+      if (this.tabList[index].refreshing) {
         // 清空列表数据
-        this.recordList = [];
+        this.tabList[index].recordList = [];
         // 重置页码
-        this.pageIndex = 1;
-        this.refreshing = false;
-        this.finished = false;
+        this.tabList[index].pageIndex = 1;
+        this.tabList[index].refreshing = false;
+        this.tabList[index].finished = false;
         // 将 loading 设置为 true，表示处于加载状态
-        this.loading = true;
+        this.tabList[index].loading = true;
       }
       // 重新加载数据
-      this.onLoad();
+      this.onLoad(index);
     }
   },
   watch: {},
